1dfaacc7171bc7f2b8b726b555f6eaddfee2487e
Alexander Gromchenko <agromcenko@gmail.com>
2023-10-29 15:07:36 +0300

    3 lesson HW

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -7,5 +7,7 @@ public abstract class Config {
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver-win64-118.0.5993.70.exe");
         Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+
     }
 }
--- src/test/java/DemoQaTest.java
+++ src/test/java/DemoQaTest.java
@@ -1,15 +1,57 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DemoQaTest extends Config {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+    }
+
     @Test
-    void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").shouldBe(visible).setValue("Alexander");
+        $("#lastName").shouldBe(visible).setValue("Gromchenko");
+        $("#userEmail").shouldBe(visible).setValue("test@gmail.com");
+        $("[for='gender-radio-1']").shouldBe(visible).click();
+        $("#userNumber").shouldBe(visible).setValue("79031234567");
+        $("#dateOfBirthInput").shouldBe(visible).click();
+        $(".react-datepicker__month-select").shouldBe(visible).click();
+        $(byValue("1")).shouldBe(visible).click();
+        $(".react-datepicker__year-select").shouldBe(visible).click();
+        $(byValue("1991")).shouldBe(visible).click();
+        $(".react-datepicker__day--020").shouldBe(visible).click();
+        $("[for='hobbies-checkbox-1'").shouldBe(visible).click();
+        $("[for='hobbies-checkbox-2'").shouldBe(visible).click();
+        $("[for='hobbies-checkbox-3'").shouldBe(visible).click();
+        $("#uploadPicture").shouldBe(visible).uploadFile(new File("picture/homer.png"));
+        $("#currentAddress").shouldBe(visible).setValue("Home Address");
+        $("#react-select-3-input").shouldBe(visible).val("NCR").pressEnter();
+        $("#react-select-4-input").shouldBe(visible).val("Delhi").pressEnter();
+        $("#subjectsInput").shouldBe(visible).setValue("Physics").pressEnter();
+        $("#submit").shouldBe(visible).pressEnter();
+
+        $(".table-responsive").shouldHave(
+                text("Alexander Gromchenko"),
+                text("test@gmail.com"),
+                text("Male"),
+                text("7903123456"),
+                text("20 February,1991"),
+                text("Physics"),
+                text("Sports, Reading, Music"),
+                text("homer.png"),
+                text("Home Address"),
+                text("NCR Delhi")
+        );
     }
 }
